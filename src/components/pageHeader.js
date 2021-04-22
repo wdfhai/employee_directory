c9da5cefb3357b10179a620f1e0d59ef7132e734
wdfhai <wd.fhai@outlook.com>
2021-04-22 15:47:32 -0500

    fixed typos

--- src/components/pageHeader.js
+++ src/components/pageHeader.js
@@ -2,8 +2,8 @@ export const PageHeader = () => {
   return(
     <div className="jumbotron jumbotron-fluid" style={{backgroundColor:"black", color:"white", marginBottom:"0"}}>
       <div className="container">
-        <h1 className="display-4" style={{textAlign:"center"}}>Employee Directory</h1>
-        <p className="lead" style={{textAlign:"center"}}>Search using the Search Field or Sort by pressing the header buttons.</p>
+        <h1 className="display-4" style={{textAlign:"center"}}>My Employee Directory</h1>
+        <p className="lead" style={{textAlign:"center"}}>Search using the Search Field or Sort by pressing the sort button.</p>
       </div>
     </div>
   )
