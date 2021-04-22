18fbc023820d19fdb9d55ae1b001264fa4ff5ca6
wdfhai <wd.fhai@outlook.com>
2021-04-22 13:36:23 -0500

    enabled github pages deployment, added readme and preview img

--- src/components/APIResults.js
+++
@@ -1,7 +0,0 @@
-export const GetData = () => {
-  return fetch("https://randomuser.me/api/?nat=us&results=100")
-   .then(response => response.json())
-   .then((response) => {
-    return response
- })
-}
